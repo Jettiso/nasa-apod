8091beea3a4d8ddcad736845aab0b03d73e3f19b
Jettison <ilmsmjettison@gmail.com>
2022-10-21 15:00:45 +0800

    save

--- nasa-apod/src/App.jsx
+++ nasa-apod/src/App.jsx
@@ -1,8 +1,8 @@
 import "./style.css";
 import DateComponent from "./components/DateComponent";
-import Main from "./components/Main";
+
 import ExpModal from "./components/ExpModal";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 
 function App() {
     const [data, setData] = useState({});
@@ -10,7 +10,7 @@ function App() {
     const dateTime = new Date().toJSON().slice(0, 10);
 
     const [time, setTime] = useState(dateTime);
-    
+
     useEffect(() => {
         fetch(
             `https://api.nasa.gov/planetary/apod?date=${time}&api_key=GLktdZldrqi7InFzth1TrqUsPxxQwhrBx4K5qA0d`
@@ -21,27 +21,34 @@ function App() {
 
     const changeHandler = (e) => {
         const value = e.target.value;
-        setTime(value)
-    }
+        setTime(value);
+    };
 
+    const [openModal, setOpenModal] = useState(false);
 
+    const MainImg = React.lazy(() => {
+        return import("./components/Main");
+    });
 
-    const [openModal, setOpenModal] = useState(false)
-
-
-    console.log(time)
     return (
         <div>
             <div className="siteWrapper">
                 <h3>Enter a date between June 16, 1995 and today:</h3>
-                <DateComponent value={time} onChangeHandler={changeHandler}  openText={setOpenModal}/>
-                <Main img={data.url} title={data.title} date={time} />
-                <div className="transparentBg">
-                {openModal && <ExpModal explanation={data.explanation} closeModal={setOpenModal}/>}
-                </div>
+                <DateComponent
+                    value={time}
+                    onChangeHandler={changeHandler}
+                    openText={setOpenModal}
+                />
+                <Suspense >
+                    <MainImg img={data.url} title={data.title} date={time} />
+                </Suspense>
+                {openModal && (
+                    <ExpModal
+                        explanation={data.explanation}
+                        closeModal={setOpenModal}
+                    />
+                )}
             </div>
-
-            {/* <Explanation explanation={data.explanation} />  */}
         </div>
     );
 }
--- nasa-apod/src/components/ExpModal.jsx
+++ nasa-apod/src/components/ExpModal.jsx
@@ -2,9 +2,11 @@ import "../style.css";
 
 function ExpModal(props) {
     return (
+        <div className="transparentBg">
         <div className="explanation">
             <p>{props.explanation}</p>
             <button onClick={() => {props.closeModal(false)}}> Close</button>
+        </div>
         </div>
     );
 }
